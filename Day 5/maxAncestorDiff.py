3d8b267ff4afe799149db7797d0f1994f11b8cda
Aishwarya Sharma <40992815+iiaishwarya@users.noreply.github.com>
2022-01-05 10:27:25 -0800

    Create maxAncestorDiff.py

---
+++ Day 5/maxAncestorDiff.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        
+        def dfs(node, curr_max, curr_min):
+            if not node:
+                return curr_max - curr_min
+            
+            curr_max = max(curr_max, node.val)
+            curr_min = min(curr_min, node.val)
+            left = dfs(node.left, curr_max, curr_min)            
+            right = dfs(node.right, curr_max, curr_min)
+            return max(left, right)
+        
+        return dfs(root, root.val, root.val)
